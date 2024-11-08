a25ea9ad28bab216075c4c0a8dd0ab9d2ba22927
Bruno <bme.silva@campus.fct.unl.pt>
2024-11-08 13:17:59 +0000

    qf

--- pyinjective/proto/descriptor.py
+++ pyinjective/proto/descriptor.py
@@ -4,4 +4,4 @@ from google.protobuf.descriptor_pool import DescriptorPool
 INJECTIVE_DESCRIPTOR_POOL = DescriptorPool()
 file_descriptor_proto = descriptor_pb2.FileDescriptorProto()
 descriptor_pb2.DESCRIPTOR.CopyToProto(file_descriptor_proto)
-INJECTIVE_DESCRIPTOR_POOL.pool.Add(file_descriptor_proto)
+INJECTIVE_DESCRIPTOR_POOL.Add(file_descriptor_proto)
